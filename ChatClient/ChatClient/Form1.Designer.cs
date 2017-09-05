7363e216aaff9b59e63a50cebae87aa5588ab714
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-09-05 15:47:57 +0200

    added classes

--- ChatClient/ChatClient/Form1.Designer.cs
+++ ChatClient/ChatClient/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.messageButton = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chatBox = new System.Windows.Forms.ListBox();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(148, 58);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(124, 23);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Connect to server";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // messageButton
+            // 
+            this.messageButton.Location = new System.Drawing.Point(148, 226);
+            this.messageButton.Name = "messageButton";
+            this.messageButton.Size = new System.Drawing.Size(124, 23);
+            this.messageButton.TabIndex = 1;
+            this.messageButton.Text = "Send message";
+            this.messageButton.UseVisualStyleBackColor = true;
+            this.messageButton.Click += new System.EventHandler(this.messageButton_Click);
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(148, 21);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(124, 20);
+            this.nameBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Chat name:";
+            // 
+            // chatBox
+            // 
+            this.chatBox.FormattingEnabled = true;
+            this.chatBox.Location = new System.Drawing.Point(38, 99);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(234, 82);
+            this.chatBox.TabIndex = 4;
+            // 
+            // messageBox
+            // 
+            this.messageBox.Location = new System.Drawing.Point(148, 200);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(124, 20);
+            this.messageBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.chatBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.messageButton);
+            this.Controls.Add(this.connectButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button messageButton;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox chatBox;
+        private System.Windows.Forms.TextBox messageBox;
     }
 }
 
--- ChatClient/ChatClient/Form1.cs
+++ ChatClient/ChatClient/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,60 @@ namespace ChatClient
 {
     public partial class Form1 : Form
     {
+        TcpClient clientSocket = new TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        string readData = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            readData = "Connected to chat server";
+            Message();
+            clientSocket.Connect("172.0.0.1", 8888);
+            serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(messageBox.Text = "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            Thread thread = new Thread(GetMessage);
+            thread.Start();
+        }
+
+        private void GetMessage()
+        {
+            while (true)
+            {
+                serverStream = clientSocket.GetStream();
+                int buffsize = 0;
+                byte[] inStream = new byte[10025];
+                buffsize = clientSocket.ReceiveBufferSize;
+                serverStream.Read(inStream, 0, buffsize);
+                string returnData = System.Text.Encoding.ASCII.GetString(inStream);
+                readData = "" + returnData;
+                Message();
+            }
+        }
+
+        private void Message()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(Message));
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void messageButton_Click(object sender, EventArgs e)
+        {
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(messageBox.Text + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
         }
     }
 }
