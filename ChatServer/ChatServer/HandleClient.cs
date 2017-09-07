a30a2e4920a1eea0abaee58241421c3a56bdc8dd
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2017-09-07 13:47:58 +0200

    branch fix server

---
+++ ChatServer/ChatServer/HandleClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    public class HandleClient
+    {
+        private TcpClient clientSocket;
+        private string id;
+        private Hashtable clientsList;
+
+        public void StartClient(TcpClient inClientSocket, string clineNo, Hashtable cList)
+        {
+            clientSocket = inClientSocket;
+            id = clineNo;
+            clientsList = cList;
+            Thread ctThread = new Thread(DoChat);
+            ctThread.Start();
+        }
+
+        private void DoChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[1024];
+            string dataFromClient = null;
+            string rCount = null;
+            requestCount = 0;
+
+            while (true)
+            {
+                try
+                {
+                    requestCount++;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                    dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine("From client - " + id + " : " + dataFromClient);
+                    rCount = Convert.ToString(requestCount);
+
+                    Program.Broadcast(dataFromClient, id, true);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -1,15 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Net;
 
 namespace ChatServer
 {
     class Program
     {
+        public static Hashtable clientsList = new Hashtable();
         static void Main(string[] args)
         {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 8888;
+            TcpListener serverSocket = new TcpListener(ipAddress, port);
+            TcpClient clientSocket = default(TcpClient);
+
+            serverSocket.Start();
+            Console.WriteLine("Server is running, listening on: " + ipAddress + ":" + port);
+            int counter = 0;
+            while (true)
+            {
+                counter++;
+                clientSocket = serverSocket.AcceptTcpClient();
+
+                byte[] bytesFrom = new byte[1024];
+                string dataFromClient = null;
+
+                NetworkStream networkStream = clientSocket.GetStream();
+                networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                clientsList.Add(dataFromClient, clientSocket);
+
+                Broadcast(dataFromClient + " joined ", dataFromClient, false);
+
+                Console.WriteLine(dataFromClient + " joined chat room ");
+                HandleClient client = new HandleClient();
+                client.StartClient(clientSocket, dataFromClient, clientsList);
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine("exit");
+            Console.ReadLine();
+        }
+
+        public static void Broadcast(string message, string userName, bool flag)
+        {
+            foreach (DictionaryEntry Item in clientsList)
+            {
+                TcpClient broadcastSocket;
+                broadcastSocket = (TcpClient)Item.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+                byte[] broadcastBytes = null;
+
+                if (flag == true)
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(userName + " says: " + message);
+                }
+                else
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(message);
+                }
+
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
+            }
         }
     }
 }
